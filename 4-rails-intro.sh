c261d567cfa3efa7cbb3732822a25552d7bd4501
Xingxing <xingxing.man@china.talkdesk.com>
2022-04-01 10:30:05 +0800

    添加 mvc

--- 4-rails-intro.sh
+++ 4-rails-intro.sh
@@ -19,4 +19,37 @@ bundle exec rails server
 
 ### 项目结构
 
-###
+### MVC
+
+## routing
+# * 每条路由规则由三个部分组成
+# 1. 网址
+# 2. controller#action
+# 3. 规则名称. 会自动生成 *_url, *_path 方法
+
+# https://guides.rubyonrails.org/v6.0/routing.html
+# root
+# match
+# get, post, ...
+# resources, resource
+# scope: module, controller, path, as
+
+bundle exec rails routes
+
+# 例子:
+# https://github.com/Talkdesk/talkdesk_main/blob/6a2a6bb743e52099865d4568c61ea496a2fcdcf2/app/representers/api/callbar/voicemail_drop_message_document_representer.rb#L22
+# https://github.com/Talkdesk/talkdesk_main/blob/master/app/controllers/application_controller.rb#L58
+
+## controller
+# https://guides.rubyonrails.org/v6.0/action_controller_overview.html
+
+# 接受请求参数
+# params
+# request.headers
+# request.env
+
+# 返回响应
+# render
+
+## model
+# ORM: https://www.mongodb.com/docs/mongoid/current/tutorials/mongoid-documents/
